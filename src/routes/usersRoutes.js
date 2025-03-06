0f511fd6001d60237bbd3c59aa08769b8764b984
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2025-03-06 19:49:39 +0200

    new

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // عرض جميع العملاء
 router.get('/', async (req, res) => {
     try {
-        const users = await UserModel.find();
+        const users = await UserModel.find().populate('assignedClients');
         res.status(200).json(users);
     } catch (error) {
         res.status(500).json({ message: error.message });
